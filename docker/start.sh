209c278f9e927bd44783505ce790777190312a66
takoyaks <iebrekaveunalliv@gmail.com>
2026-09-19 17:04:59 +0800

    new

--- docker/start.sh
+++ docker/start.sh
@@ -21,8 +21,6 @@ php artisan storage:link || true
 echo "Running database migrations..."
 php artisan migrate --force
 
-echo "Seeding initial database data..."
-php artisan db:seed --force
 
 echo "Optimizing Laravel..."
 php artisan optimize
